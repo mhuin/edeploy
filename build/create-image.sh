99334948bde25ac1857484b2f432689591423add
Matthieu Huin <mhu@enovance.com>
2015-01-07 10:36:40 +0100

    Revert "Add "verbose" option to rsync calls"
    This reverts commit 2966e809b036b97970b741fa6550cb6cd2d7c8a9.

--- build/create-image.sh
+++ build/create-image.sh
@@ -220,7 +220,7 @@ trap do_cleanup 0
 # Copy the data
 
 if [ -d "$DIR" ]; then
-    rsync -v -a "$DIR/" "$MDIR/"
+    rsync -a "$DIR/" "$MDIR/"
 else
     tar xf "$DIR" -C "$MDIR"
 fi
@@ -257,7 +257,7 @@ fi
 
 # Let's create a copy of the current /dev
 mkdir -p "${MDIR}/"/dev/pts
-rsync -v -a --delete-before --exclude=shm /dev/ ${MDIR}/dev/
+rsync -a --delete-before --exclude=shm /dev/ ${MDIR}/dev/
 
 # Mount /proc
 mount -t proc none "$MDIR/"proc
